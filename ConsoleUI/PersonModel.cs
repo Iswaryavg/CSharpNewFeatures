38eb2e18173ccd5bfa5b232c70bc662bda2d7e26
Iswaryavg <61296893+Iswaryavg@users.noreply.github.com>
2021-01-02 16:22:36 +0000

    Add project files.

---
+++ ConsoleUI/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class PersonModel
+    {
+        public int Id { get; init; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public PersonModel()
+        {
+
+        }
+        public PersonModel(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using ConsoleUI;
+using System;
+
+
+Console.WriteLine("Hello World!");
+Console.WriteLine(Add(4, 2));
+
+Console.ReadLine();
+
+PersonModel p = new() { Id = 1, FirstName = "Ish", LastName = "Gopu" };
+
+PersonModel p2 = new(2, "krish", "vijay");
+
+PersonModel p3 = null;
+if(p3 is null)
+{
+    Console.WriteLine("The value of p3 is not yet set");
+}
+else
+{
+    Console.WriteLine($"Hello {p.FirstName} {p.LastName} ({p.Id})");
+}
+if(p3 is not null)
+{
+    Console.WriteLine($"Hello {p.FirstName} {p.LastName} ({p.Id})");
+}
+p.FirstName = "Iswarya";
+Console.WriteLine($"Hello {p.FirstName} {p.LastName} ({p.Id})");
+static double Add(double x, double y)
+{
+    return x + y;
+}
+
+
+
